7ab939cdb652a7e9e166c87f97bdf6e0d10baaf0
David Karoly <karolyd577@gmail.com>
2022-09-18 19:46:07 +0200

    arm: save FPU regs on setjmp/longjmp
    Change-Id: I68dfed54e21c90310fb12bf91ee48a8a8d21ba9b

--- headers/posix/arch/arm/arch_setjmp.h
+++ headers/posix/arch/arm/arch_setjmp.h
@@ -1,12 +1,12 @@
 /*
- * Copyright 2008-2012 Haiku, Inc. All Rights Reserved.
+ * Copyright 2008-2022 Haiku, Inc. All Rights Reserved.
  * Distributed under the terms of the MIT License.
  */
 #ifndef _ARCH_SETJMP_H_
 #define _ARCH_SETJMP_H_
 
 
-#define _SETJMP_BUF_SZ ((15+2)*4)
+#define _SETJMP_BUF_SZ (11 + 1 + 2*8)
 typedef int __jmp_buf[_SETJMP_BUF_SZ];
 
 #endif	/* _ARCH_SETJMP_H_ */
